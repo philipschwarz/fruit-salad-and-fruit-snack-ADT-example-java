09b23da97917d8be4391756577d4f82f0e5e4146
philipschwarz <philip.johann.schwarz@gmail.com>
2022-08-07 10:59:00 +0100

    add comment in shell script

--- compile-and-run.sh
+++ compile-and-run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Download the jdk from https://jdk.java.net/19/
 PATH=~/Downloads/jdk-19.jdk/Contents/Home/bin/:$PATH
 
 javac src/fruit/adt/example/Main.java -d target --enable-preview -source 19
